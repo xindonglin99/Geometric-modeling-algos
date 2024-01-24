c28bcb5a081b113699000f7e610265c79a4ee6e2
Hans <hans_lin99@outlook.com>
2024-01-24 11:50:44 -0800

    initial update for A1

--- src/minimesh/gui/main.cpp
+++ src/minimesh/gui/main.cpp
@@ -190,7 +190,7 @@ int main(int argc, char * argv[])
 	// Change the hardcoded address to your needs.
 	if(argc == 1)
 	{
-		foldertools::makeandsetdir("D:/UBC/TA/2021Fall-524/code/CPSC524/mesh");
+		foldertools::makeandsetdir("/Users/Hans/Documents/CPSC524-modeling/mesh");
 		mohe::Mesh_io(globalvars::mesh).read_auto("camel.obj");
 	}
 	else // otherwise use the address specified in the command line
